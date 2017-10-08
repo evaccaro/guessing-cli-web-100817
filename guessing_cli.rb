8ebb907990ceefc8cf08e21ce2bc080ea31fee4f
Elisa Vaccaro <erv230@nyu.edu>
2017-10-08 22:29:33 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -12,7 +12,7 @@ def run_guessing_game
       puts "The computer guessed #{number}."
     end
 
-    puts "Guess a number between 1 and 6.\n"
+    puts "Guess a number between 1 and 6."
     number = rand(1..6).to_s
     input = gets.chomp
   end
